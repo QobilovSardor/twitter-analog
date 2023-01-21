ec1581a1d93dd427aa95ae40b3d6a8436a6c597a
SardorDev <sardorqobilov555@gmail.com>
2023-01-21 12:19:19 +0500

    loyihani like va star bo'limi bajarildi qisman

--- src/components/PostListItem/PostListItem.js
+++ src/components/PostListItem/PostListItem.js
@@ -1,26 +1,52 @@
-import './PostListItem.css'
+import React from 'react';
+import './PostListItem.css';
 
-const PostListItem = ({label, important}) => {
-  let classNames = 'app-list-item d-flex justify-content-between'
-  if (important) {
-    classNames += ' important'
+class PostListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      important: false,
+      liked: false
+    };
+    this.onStartHendler = this.onStartHendler.bind(this)
+    this.onLikedHendler = this.onLikedHendler.bind(this)
   }
-  return (
-    <div className={classNames}>
-      <span className="app-list-item-label">
-        {label}
-      </span>
-      <div className="d-flex justify-content-center align-items-center">
-        <button type="submit" className="btn-star btn-sm">
-          <i className="fa fa-star"></i>
-        </button>
-        <button type="submit" className="btn-trash btn-sm">
-          <i className="fa fa-trash"></i>
-        </button>
-        <i className="fa fa-heart"></i>
+
+  onStartHendler() {
+    this.setState(({important}) => ({important: !important}))
+  }
+
+  onLikedHendler() {
+    this.setState(({liked}) => ({liked: !liked}))
+  }
+
+  render() {
+    const { important, liked } = this.state
+    const { label } = this.props;
+    let classNames = 'app-list-item d-flex justify-content-between'
+    if (important) {
+      classNames += ' important'
+    }
+    if (liked) {
+      classNames += ' like'
+    }
+    return (
+      <div className={classNames}>
+        <span className="app-list-item-label" onClick={this.onLikedHendler}>
+          {label}
+        </span>
+        <div className="buttons d-flex justify-content-center align-items-center">
+          <button type="submit" className="btn-star btn-sm" onClick={this.onStartHendler}>
+            <i className="fa fa-star"></i>
+          </button>
+          <button type="submit" className="btn-trash btn-sm">
+            <i className="fa fa-trash"></i>
+          </button>
+          <i className="fa fa-heart"></i>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default PostListItem;
